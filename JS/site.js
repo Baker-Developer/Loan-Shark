5be789d07e07ca5dde5bff9000d2e6c1656b08be
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-07-10 18:21:34 -0500

    More Changes Tonight (Possible)

--- JS/site.js
+++ JS/site.js
@@ -1,4 +1,36 @@
 /**
  * This is a application that takes in three parameters 
+ * The amount of money loaned in dollars (balance)
+ * The time over which the loan will be repaid, in months (term)
+ * The percentage rate at which interest will accrue on the loan (rate)
+ * 
  * 
  */
+
+
+// write a function to get the inputs of the user Loan Amount, Payments, Rate
+function getValues() {
+    document.getElementById('btnSubmit');
+    document.getElementById('loanAmount');
+    document.getElementById('amountOfMonths');
+    document.getElementById('interestRate');
+
+    // if (Document.parse(loanAmount) && Document.parse(amountOfMonths) && Document.parse(interestRate)) {
+
+    // } else {
+    //     alert('Enter A Number')
+    // }
+
+}
+
+// write a function to calculate the values
+function calculateValues() {
+    
+}
+
+
+// write a function that outputs the numbers
+function displayValues() {
+    
+}
+
