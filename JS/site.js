96fb03dbd4179173a296edaf3f1719baeb510b28
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-07-13 18:18:54 -0500

    Will Come Back To This Problem

--- JS/site.js
+++ JS/site.js
@@ -1,3 +1,75 @@
+// write a function to get the inputs of the user Loan Amount, Payments, Rate
+function getValues() {
+
+    let balance = document.getElementById('loanAmount').value;
+    let term = document.getElementById('amountOfPayments').value;
+    let rate = document.getElementById('interestRate').value;
+
+    balance = parseInt(balance);
+    term = parseInt(term);
+    rate = parseInt(rate);
+
+    if (Number.isInteger(balance) && Number.isInteger(term) && Number.isInteger(rate)) {
+        // call calculateValues
+        let numbers = calculateValues(balance, term, rate);
+        //call displayValues
+        displayValues(numbers);
+    } else {
+        alert("Enter Intergers Only");
+    }
+
+}
+
+// write a function to calculate the values 
+function calculateValues(balance, term, rate) {
+
+    let returnArray = [];
+
+    // write a for loop that loops from 1 to the amountOfPayments
+    for (let i = 0; returnArray <= term; i++) {
+        if (returnArray.push(i) == true) {
+            balance * (rate / 1200) / 1(1 - (1 + rate / 1200)(-term))
+        }
+        returnArray.push(i);
+    }
+
+    // balance * (rate/1200) / 1 (1 - (1 + rate/1200)(-term))
+    // calculate the payment amount in a for loop using this formula (loanAmount) * (interestRate/1200) / (1 - (1 + interestRate/1200)(-amountOfPayments))
+
+
+
+    // the principal needs to be looped using the forumula above - interest payment 
+
+
+
+
+
+    // the interest need to be looped the previous remaining balance * interestRate / 1200
+
+    // the end of the month loanAmount = Previous Remaining Balance - principal payments note: refer to line 28 for more details
+
+    // the remaining loan balance at the end of the month needs to be looped is equal to the amount of loanAmount
+
+}
+
+
+// write a function that outputs the numbers
+function displayValues() {
+
+    // output the month 
+    // output the payment  
+
+    // output the principal 
+
+    //output the interest 
+    //output the total interest 
+    // output the balance
+
+
+}
+
+
+
 /**
  * This is a application that takes in three parameters 
  * The amount of money loaned in dollars (balance)
@@ -6,45 +78,3 @@
  * 
  * 
  */
-
-
-// write a function to get the inputs of the user Loan Amount, Payments, Rate
-function getValues() {
-
-    let loanAmount = document.getElementById('loanAmount');
-    let amountOfPayments = document.getElementById('amountOfPayments');
-    let interestRate = document.getElementById('interestRate');
-
-
-
-}
-
-// write a function to calculate the values 
-function calculateValues() {
-    // write a for loop that loops from 1 to the amountOfPayments
-
-    // output the payment amount in a for loop using this formula (loanAmount) * (interestRate/1200) / (1 - (1 + interestRate/1200)(-amountOfPayments))
-
-    // the principal needs to be looped using the forumula above - interest payment 
-
-    // the interest need to be looped the previous remaining balance * interestRate / 1200
-
-    // the end of the month loanAmount = Previous Remaining Balance - principal payments note: refer to line 28 for more details
-
-    // the remaining loan balance at the end of the month needs to be looped is equal to the amount of loanAmount
-
-
-}
-
-
-// write a function that outputs the numbers
-function displayValues() {
-    // let testnum = "1";
-    // let test = "$125.00";
-    // document.getElementById('message').innerHTML = `<tr><th scope="row">${testnum}</th>
-    //  <td>${test}</td><td>${test}</td><td>${test}</td><td>${test}</td><td>${test}</td></tr>`;
-
-
-
-
-}
