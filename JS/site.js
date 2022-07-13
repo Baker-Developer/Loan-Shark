ed992ad5f6725c7b9e581dccd767b4ef41159e4b
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-07-12 23:39:37 -0500

    More Sudo Code Will Correct Tommorow

--- JS/site.js
+++ JS/site.js
@@ -10,27 +10,41 @@
 
 // write a function to get the inputs of the user Loan Amount, Payments, Rate
 function getValues() {
-    document.getElementById('btnSubmit');
-    document.getElementById('loanAmount');
-    document.getElementById('amountOfMonths');
-    document.getElementById('interestRate');
 
-    // if (Document.parse(loanAmount) && Document.parse(amountOfMonths) && Document.parse(interestRate)) {
+    let loanAmount = document.getElementById('loanAmount');
+    let amountOfPayments = document.getElementById('amountOfPayments');
+    let interestRate = document.getElementById('interestRate');
 
-    // } else {
-    //     alert('Enter A Number')
-    // }
+
 
 }
 
-// write a function to calculate the values
+// write a function to calculate the values 
 function calculateValues() {
-    
+    // write a for loop that loops from 1 to the amountOfPayments
+
+    // output the payment amount in a for loop using this formula (loanAmount) * (interestRate/1200) / (1 - (1 + interestRate/1200)(-amountOfPayments))
+
+    // the principal needs to be looped using the forumula above - interest payment 
+
+    // the interest need to be looped the previous remaining balance * interestRate / 1200
+
+    // the end of the month loanAmount = Previous Remaining Balance - principal payments note: refer to line 28 for more details
+
+    // the remaining loan balance at the end of the month needs to be looped is equal to the amount of loanAmount
+
+
 }
 
 
 // write a function that outputs the numbers
 function displayValues() {
-    
-}
+    // let testnum = "1";
+    // let test = "$125.00";
+    // document.getElementById('message').innerHTML = `<tr><th scope="row">${testnum}</th>
+    //  <td>${test}</td><td>${test}</td><td>${test}</td><td>${test}</td><td>${test}</td></tr>`;
 
+
+
+
+}
