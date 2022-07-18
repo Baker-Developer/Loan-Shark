2fa40e8fa243cdd268302ca50c212e38f3097794
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-07-18 14:57:38 -0500

    Changes To JS And HTML
    Sudo Code In JS and Fixed Mobile Layout  On Landing Page

--- JS/site.js
+++ JS/site.js
@@ -1,80 +1,56 @@
-// write a function to get the inputs of the user Loan Amount, Payments, Rate
-function getValues() {
-
-    let balance = document.getElementById('loanAmount').value;
-    let term = document.getElementById('amountOfPayments').value;
-    let rate = document.getElementById('interestRate').value;
-
-    balance = parseInt(balance);
-    term = parseInt(term);
-    rate = parseInt(rate);
-
-    if (Number.isInteger(balance) && Number.isInteger(term) && Number.isInteger(rate)) {
-        // call calculateValues
-        let numbers = calculateValues(balance, term, rate);
-        //call displayValues
-        displayValues(numbers);
-    } else {
-        alert("Enter Intergers Only");
-    }
-
-}
-
-// write a function to calculate the values 
-function calculateValues(balance, term, rate) {
-
-    let returnArray = [];
-
-    // write a for loop that loops from 1 to the amountOfPayments
-    for (let i = 0; returnArray <= term; i++) {
-        if (returnArray.push(i) == true) {
-            balance * (rate / 1200) / 1(1 - (1 + rate / 1200)(-term))
-        }
-        returnArray.push(i);
-    }
-
-    // balance * (rate/1200) / 1 (1 - (1 + rate/1200)(-term))
-    // calculate the payment amount in a for loop using this formula (loanAmount) * (interestRate/1200) / (1 - (1 + interestRate/1200)(-amountOfPayments))
-
-
-
-    // the principal needs to be looped using the forumula above - interest payment 
-
-
-
-
-
-    // the interest need to be looped the previous remaining balance * interestRate / 1200
-
-    // the end of the month loanAmount = Previous Remaining Balance - principal payments note: refer to line 28 for more details
-
-    // the remaining loan balance at the end of the month needs to be looped is equal to the amount of loanAmount
-
-}
-
-
-// write a function that outputs the numbers
-function displayValues() {
-
-    // output the month 
-    // output the payment  
-
-    // output the principal 
-
-    //output the interest 
-    //output the total interest 
-    // output the balance
-
-
-}
-
-
+/**
+ * Total Monthly Payment = (amount loaned) * (rate/1200) / (1 – (1 + rate/1200)(-Number of Months) )
+    Remaining Balance before the very first month equals the amount of the loan.
+    Interest Payment = Previous Remaining Balance * rate/1200
+    Principal Payment = Total Monthly Payment - Interest Payment
+    At end each month, Remaining Balance = Previous Remaining Balance - principal payments
+ */
 
 /**
- * This is a application that takes in three parameters 
- * The amount of money loaned in dollars (balance)
- * The time over which the loan will be repaid, in months (term)
- * The percentage rate at which interest will accrue on the loan (rate)
- * 
- * 
+/*
+    *  The output should include the following:
+    1. The month (1 corresponding to the 1st
+    month of payment, through the total
+    number of months)
+    2. The payment amount
+    3. The principal paid this month
+    4. The interest paid this month
+    5. The total interest paid to date
+    6. The remaining loan balance at the end of
+    the month 
  */
+
+
+// write a function that takes in the id of balance , months and rate
+function getInput() {
+    // Get the input of Loan Amount
+    let loanAmount = document.getElementById('balance').value;
+    // Get the input of Payments
+    let term_InMonths = document.getElementById('term').value;
+    // Get the input of Rate
+    let interestRate = document.getElementById('rate').value;
+
+    loanAmount = parseInt(loanAmount);
+    term_InMonths = parseInt(term_InMonths);
+    interestRate = parseInt(interestRate);
+
+    // parse user inputs into integers
+    if (Number.isInteger(loanAmount) && Number.isInteger(term_InMonths) && Number.isInteger(interestRate)) {
+
+
+    } else {
+        alert('enter a valid number')
+    }
+
+    for (let i = 1; i <= term_InMonths; i++) {
+        // the next out is the total monthly
+        let test = loanAmount * interestRate / 1200 / 1 - 1 + interestRate / 1200 - term_InMonths
+        console.log(test + 'dollars');
+    }
+
+}
+
+function output() {
+
+
+}
