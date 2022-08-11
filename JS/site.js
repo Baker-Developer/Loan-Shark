deaebc15f7dfd00a077a7437f79483388e1b5518
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-08-11 13:30:25 -0500

    Edited Code HTML & JS 👍

--- JS/site.js
+++ JS/site.js
@@ -1,45 +1,38 @@
-// Get the input of the user
 function getInput() {
-    let LoanAmount = document.getElementById('balance').value;
-    let NumberOfPayments = document.getElementById('term').value;
-    let IntrestRate = document.getElementById('rate').value;
+    let Balance = document.getElementById('balance').value;
+    let Term = document.getElementById('term').value;
+    let Rate = document.getElementById('rate').value;
 
-    // Parse into integers
-    if (Number.parseInt(LoanAmount) && Number.parseInt(NumberOfPayments) && Number.parseInt(IntrestRate)) {
-        // call calculateValues method
-        let calcValues = calculateValues();
-        // call displayValues method and write the values to the user
-        display = displayValues(calcValues);
-    } else {
-        // alert the user to input a number
-        alert('Enter A Number');
-    }
+    // Parse data into integers
+    Balance = parseInt(Balance);
+    Term = parseInt(Term);
+    Rate = parseInt(Rate);
 
-}
-
-// Calculate the numbers
-function calculateValues(LoanAmount, NumberOfPayments, IntrestRate) {
     /**
-         * 
-         * TotalMonthlyPayment = (LoanAmount) * (IntrestRate/1200) / (1 – (1 + IntrestRate/1200)(-NumberOfPayments) )
-            RemainingBalance = LoanAmount.
-            InterestPayment = LoanAmount * IntrestRate/1200
-            PrincipalPayment = TotalMonthlyPayment - InterestPayment
-            At end each month, LoanAmount = LoanAmount - PrincipalPayment
-        * 
-     */
-    let length = [];
-
-    for (let i = 1; i <= length.pop; i++) {
-        console.log(i);
-    }
+ * 
+        Total Monthly Payment = (amount loaned) * (rate/1200) / (1 – (1 + rate/1200)(-Number of Months) ) 
+        Remaining Balance before the very first month equals the amount of the loan.
+        Interest Payment = Previous Remaining Balance * rate/1200
+        Principal Payment = Total Monthly Payment - Interest Payment
+        At end each month, Remaining Balance = Previous Remaining Balance - principal payments 
+ */
+    
 
 
 
-}
+    /**
+     * The output should include the following:
+    1. The month (1 corresponding to the 1st
+    month of payment, through the total
+    number of months)
+    2. The payment amount
+    3. The principal paid this month
+    4. The interest paid this month
+    5. The total interest paid to date
+    6. The remaining loan balance at the end of
+    the month
+ */
 
-
-// Display the values
-function displayValues() {
+    //Output the month in a for loop starting at 1 and ending at the Rate end
 
 }
