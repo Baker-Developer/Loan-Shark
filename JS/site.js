bd2515f00bc45eb78a335518017482a452b00ab9
Baker-Developer <benjaminbaker1122@gmail.com>
2022-10-25 00:19:17 -0500

    More work

--- JS/site.js
+++ JS/site.js
@@ -21,44 +21,58 @@ function getInput() {
     } else {
         alert('Enter a integer');
     }
+
 }
 
 function calculateValues(loanBalance, loanTerm, loanRate) {
 
     let numbers = [];
 
-    // loop from first month 1 to the user selected month
-    for (let i = 1; i <= loanTerm; i++) {
-        numbers.push(i);
-    }
+
 
     // calculate the current remaining balance for the before calculations of months 
     let previousRemainingBalance = loanBalance;
 
     // calculate the totalMonthlyPayment for the amount of months
     let totalMonthlyPayment = (loanBalance) * (loanRate / 1200) / (1 - (1 + loanRate / 1200) ** ((-loanTerm)))
-    console.log(Math.round(totalMonthlyPayment));
-    // numbers.push(`total monthly payment ` + Math.round(totalMonthlyPayment));
+    console.log(Math.round(totalMonthlyPayment)); // Second On The List
 
     // calculate the interestPayment for the amount of months
     let interestPayment = previousRemainingBalance * loanRate / 1200;
-    console.log(Math.round(interestPayment));
+    console.log(Math.round(interestPayment)); // Fourth On The List
     // numbers.push(`Interest Payment ` + Math.round(interestPayment));
 
     // calculate the principalPayment for the amount of months
     let principalPayment = totalMonthlyPayment - interestPayment;
-    console.log(Math.round(principalPayment));
+    console.log(Math.round(principalPayment)); // Third on the list
     // numbers.push(`total Principal Payment ` + Math.round(principalPayment));
 
     // calculate the totalInterest for the amount of months 
     let totalInterest = (loanBalance + interestPayment) - loanBalance;
-    console.log(Math.round(totalInterest));
+    console.log(Math.round(totalInterest)); // Fifth  On The List
     // numbers.push(`total Interest Payment ` + Math.round(principalPayment));
 
     // calculate the current remaining balance for the amount of months 
-    let currentRemainingBalance = previousRemainingBalance - principalPayment;
+    let currentRemainingBalance = previousRemainingBalance - principalPayment; // Last On The List
     console.log(Math.round(currentRemainingBalance));
     // numbers.push(`total Current Balance ` + Math.round(principalPayment));
+
+   
+
+    let TotalMonthlyPaymentPerMonth = totalMonthlyPayment; // Second On The List
+    let PaymentPricipalPerMonth = principalPayment; // Third on the list
+    let MainInterestPerMonth = interestPayment;  // Fourth On The List
+    let ToalInterestPerMonth = totalInterest; // Fifth  On The List
+    let CurrentBalancePerMonth = currentRemainingBalance; // Last On The List
+
+     // loop from first month 1 to the user selected month
+     for (let i = 1; i <= loanTerm; i++) {
+        numbers.push(i); // First On The List
+        // Calculate the payment based on the current balance
+        CurrentBalancePerMonth--;
+        console.log(Math.round(CurrentBalancePerMonth));
+    }
+    
 
     return numbers;
 }
@@ -77,5 +91,6 @@ function displayValues(calculatedNumbers) {
 
 
     document.getElementById('results').innerHTML = templateRow;
+
 
 }
